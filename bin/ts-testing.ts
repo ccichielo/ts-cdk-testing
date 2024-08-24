ab5794dfa9588e1c727d9e458599f36e3795f8f2
Christopher Cichielo <chris.cichielo@gmail.com>
2024-08-24 13:34:35 -0400

    tagging

--- bin/ts-testing.ts
+++ bin/ts-testing.ts
@@ -4,5 +4,8 @@ import * as cdk from "aws-cdk-lib";
 import { TsSimpleStack } from "../lib/ts-testing-stack";
 
 const app = new cdk.App();
-new TsSimpleStack(app, "TsTestingStack", {});
-
+const simpleStack = new TsSimpleStack(app, "TsTestingStack", {});
+cdk.Tags.of(simpleStack).add("stage", "test");
+cdk.Tags.of(simpleStack).add("storage", "main", {
+  includeResourceTypes: ["AWS::S3::Bucket"],
+});
