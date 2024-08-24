40df591d8bb21c3b0e361b8138a581f966a74f27
Christopher Cichielo <chris.cichielo@gmail.com>
2024-08-24 13:44:26 -0400

    Aspect example

--- bin/ts-testing.ts
+++ bin/ts-testing.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { TsSimpleStack } from "../lib/ts-testing-stack";
+import { PolicyChecker } from "../lib/PolicyChecker";
 
 const app = new cdk.App();
 const simpleStack = new TsSimpleStack(app, "TsTestingStack", {});
@@ -9,3 +9,5 @@ cdk.Tags.of(simpleStack).add("stage", "test");
 cdk.Tags.of(simpleStack).add("storage", "main", {
   includeResourceTypes: ["AWS::S3::Bucket"],
 });
+
+cdk.Aspects.of(app).add(new PolicyChecker());
